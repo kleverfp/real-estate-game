a4eb5db0dd3424b3f5263188999ac4c56b68e260
Klever Ferreira <kleverfp@gmail.com>
2023-03-01 11:23:50 -0300

    feat: add script to run migrations and seeders

--- src/database/seeders/20230301012152-init-building.js
+++ src/database/seeders/20230301012152-init-building.js
@@ -6,102 +6,102 @@ module.exports = {
     await queryInterface.bulkInsert('buildings', [
       {
         sell_value: 50,
-        rent_value: 3,
+        rent_value: 38,
         position:1
       },
       {
         sell_value: 70,
-        rent_value: 4,
+        rent_value: 42,
         position:2
       },
       {
         sell_value: 90,
-        rent_value: 5,
+        rent_value: 19,
         position:3
       },
       {
         sell_value: 60,
-        rent_value: 3,
+        rent_value: 36,
         position:4
       },
       {
         sell_value: 50,
-        rent_value: 3,
+        rent_value: 15,
         position:5
       },
       {
         sell_value: 60,
-        rent_value: 4,
+        rent_value: 41,
         position:6
       },
       {
         sell_value: 50,
-        rent_value: 3,
+        rent_value: 23,
         position:7
       },
       {
         sell_value: 80,
-        rent_value: 6,
+        rent_value: 36,
         position:8
       },
       {
         sell_value: 90,
-        rent_value: 5,
+        rent_value: 25,
         position:9
       },
       {
         sell_value: 50,
-        rent_value: 4,
+        rent_value: 42,
         position:10
       },
       {
         sell_value: 50,
-        rent_value: 3,
+        rent_value: 33,
         position:11
       },
       {
         sell_value: 70,
-        rent_value: 5,
+        rent_value: 35,
         position:12
       },
       {
         sell_value: 90,
-        rent_value: 4,
+        rent_value: 54,
         position:13
       },
       {
         sell_value: 70,
-        rent_value: 3,
+        rent_value: 23,
         position:14
       },
       {
         sell_value: 60,
-        rent_value: 2,
+        rent_value: 32,
         position:15
       },
       {
         sell_value: 50,
-        rent_value: 3,
+        rent_value: 17,
         position:16
       },
       {
         sell_value: 90,
-        rent_value: 5,
+        rent_value: 53,
         position:17
       },
       {
         sell_value: 60,
-        rent_value: 2,
+        rent_value: 25,
         position:18
       },
       {
         sell_value: 80,
-        rent_value: 5,
+        rent_value: 52,
         position:19
       },
       {
         sell_value: 90,
-        rent_value: 7,
+        rent_value: 17,
         position:20
       }
     ], {});
